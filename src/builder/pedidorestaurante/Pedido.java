d3e7b91d693b2d51637162a7d6bc0e1e7b1078c7
Judson Paiva <judson.paiva@hotmail.com>
2026-01-17 23:56:06 +0100

    padrao builder - implementando pedido restaurante

---
+++ src/builder/pedidorestaurante/Pedido.java
@@ -0,0 +1,13 @@
+package builder.pedidorestaurante;
+
+public class Pedido {
+    private final String pratoPrincipal;
+    private final String bebida;
+    private final String sobremesa;
+    private final String acompanhamento;
+    private final boolean paraViagem;
+
+
+
+
+}
