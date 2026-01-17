7b06ab0b62b5ce74c8f40a610e4bcddfc97cdda3
Judson Paiva <judson.paiva@hotmail.com>
2026-01-18 00:04:28 +0100

    padrao builder - implementando pedido restaurante classe Pedido

--- src/builder/pedidorestaurante/Pedido.java
+++ src/builder/pedidorestaurante/Pedido.java
@@ -7,7 +7,45 @@ public class Pedido {
     private final String acompanhamento;
     private final boolean paraViagem;
 
+    public Pedido(PedidoBuilder builder) {
+        this.pratoPrincipal = builder.pratoPrincipal;
+        this.bebida = builder.bebida;
+        this.sobremesa = builder.sobremesa;
+        this.acompanhamento = builder.acompanhamento;
+        this.paraViagem = builder.paraViagem;
+    }
 
 
+    public static class PedidoBuilder {
+        private final String pratoPrincipal;
+        private String bebida = "";
+        private String sobremesa = "";
+        private String acompanhamento = "";
+        private boolean paraViagem = false;
+
+        public PedidoBuilder(String pratoPrincipal) {
+            this.pratoPrincipal = pratoPrincipal;
+        }
+
+        public PedidoBuilder bebida(String bebida) {
+            this.bebida = bebida;
+            return this;
+        }
+
+        public PedidoBuilder sobremesa(String sobremesa) {
+            this.sobremesa = sobremesa;
+            return this;
+        }
+
+        public PedidoBuilder acompanhamento(String acompanhamento) {
+            this.acompanhamento = acompanhamento;
+            return this;
+        }
+
+        public PedidoBuilder paraViagem(boolean paraViagem) {
+            this.paraViagem = paraViagem;
+            return this;
+        }
+    }
 
 }
