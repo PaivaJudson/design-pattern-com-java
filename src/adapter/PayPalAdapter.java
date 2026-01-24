197389571bfdf1ced4d024c85d369e4ec6f04574
Judson Paiva <judson.paiva@hotmail.com>
2026-01-24 22:28:04 +0100

    adpater

--- src/adapter/PayPalAdapter.java
+++ src/adapter/PayPalAdapter.java
@@ -1,4 +1,20 @@
 package adapter;
 
-public class PayPalAdapter {
+public class PayPalAdapter implements GatewayPagamento {
+
+    private PayPalAPI paypal;
+
+    public PayPalAdapter(PayPalAPI paypal) {
+        this.paypal = paypal;
+    }
+
+    @Override
+    public void pagar(double valor) {
+        paypal.enviarPagamento(valor);
+    }
+
+    @Override
+    public boolean autorizar() {
+        return paypal.verificarConexao();
+    }
 }
--- src/adapter/StripeAdapter.java
+++ src/adapter/StripeAdapter.java
@@ -1,4 +1,20 @@
 package adapter;
 
-public class StripeAdapter {
+public class StripeAdapter implements GatewayPagamento {
+
+    private StripeAPI stripe;
+
+    public StripeAdapter(StripeAPI stripe){
+        this.stripe = stripe;
+    }
+
+    @Override
+    public void pagar(double valor) {
+        stripe.processarCobranca(valor);
+    }
+
+    @Override
+    public boolean autorizar() {
+        return stripe.validarAPIKey();
+    }
 }
